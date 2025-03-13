6a44c783e288a55f095259abe5422d31bbf979bd
SerhiiQA <nabukve@gmail.com>
2025-03-13 21:31:15 +0200

    debug

--- tests/test/mainLoginPage.spec.ts
+++ tests/test/mainLoginPage.spec.ts
@@ -14,7 +14,6 @@ test.describe('Login Validation', () => {
         await mainPage.fillLoginDetails('TestLogin_', 'Password1@');
         await mainPage.clickLoginButton();
 
-        // Перевірка наявності "Logout" кнопки після успішного входу
         const logoutButton = mainPage.getLogoutButtonLocator();
         await expect(logoutButton).toBeVisible();
     });
@@ -24,7 +23,6 @@ test.describe('Login Validation', () => {
         await mainPage.fillLoginDetails('', validPassword); 
         await mainPage.clickLoginButton();
 
-        // Перевірка повідомлення про заповнення поля "Login"
         const loginMessage = await mainPage.getLoginFieldValidationMessage();
         expect(loginMessage.toLowerCase()).toContain('fill out this field');
     });
@@ -34,7 +32,6 @@ test.describe('Login Validation', () => {
         await mainPage.fillLoginDetails(validLogin, ''); 
         await mainPage.clickLoginButton();
 
-        // Перевірка повідомлення про заповнення поля "Password"
         const passwordMessage = await mainPage.getPasswordFieldValidationMessage();
         expect(passwordMessage.toLowerCase()).toContain('fill out this field');
     });
@@ -43,7 +40,6 @@ test.describe('Login Validation', () => {
         await mainPage.fillLoginDetails('', ''); 
         await mainPage.clickLoginButton();
 
-        // Перевірка повідомлення про заповнення поля "Login"
         const loginMessage = await mainPage.getLoginFieldValidationMessage();
         expect(loginMessage.toLowerCase()).toContain('fill out this field');
     });
@@ -54,59 +50,7 @@ test.describe('Login Validation', () => {
         await mainPage.fillLoginDetails(invalidLogin, invalidPassword);
         await mainPage.clickLoginButton();
 
-        // Перевірка повідомлення про помилкові логін/пароль
         const warningText = await mainPage.getWarningLabelText();
         expect(warningText?.trim()).toBe('Invalid username/password');
     });
 });
-
-
-
-// import { test, expect } from '@playwright/test';
-// import { MainPage } from '../page/MainPage.ts';
-// import { faker } from '@faker-js/faker'; 
-
-// test.describe('Login Validation', () => {
-//     let mainPage;
-
-//     test.beforeEach(async ({ page }) => {
-//         mainPage = new MainPage(page); 
-//         await mainPage.goto(); 
-//     });
-
-//     test('Login with valid data / TC 1', async () => {
-//         await mainPage.fillLoginDetails('TestLogin_', 'Password1@');
-//         await mainPage.clickLoginButton();
-//         await mainPage.clickLogoutButton();
-//     });
-
-//     test('Login with empty Login field / TC 2', async () => {
-//         const validPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
-         
-//         await mainPage.fillLoginDetails('', validPassword); 
-//         await mainPage.clickLoginButton();
-//         await mainPage.errorMessageLogin();
-//     });
-
-//     test('Login with empty Password field / TC 3', async () => {
-//         const validLogin = faker.string.alphanumeric(8); 
-//         await mainPage.fillLoginDetails(validLogin, ''); 
-//         await mainPage.clickLoginButton();
-//         await mainPage.errorMessagePassword();
-//     });
-
-//     test('Login with empty fields / TC 4', async () => {
-//         await mainPage.fillLoginDetails('', ''); 
-//         await mainPage.clickLoginButton();
-//         await mainPage.errorMessageLogin();
-//     });
-
-//     test('Login with invalid data / TC 5', async () => {
-//         const invalidLogin = faker.string.alphanumeric(8); 
-//         const invalidPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
-
-//         await mainPage.fillLoginDetails(invalidLogin, invalidPassword);
-//         await mainPage.clickLoginButton();
-//         await mainPage.checkWarningLabel();
-//     });
-// });
