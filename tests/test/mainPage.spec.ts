c013f27948f482042f42cca09b7695280ce848ab
SerhiiQA <nabukve@gmail.com>
2025-03-13 12:50:58 +0200

    tc debug

--- tests/test/mainPage.spec.ts
+++ tests/test/mainPage.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { MainPage } from '../page/MainPage.ts';
 import { faker } from '@faker-js/faker'; 
 
-test.describe('Login Page Validation', () => {
+test.describe('Login Validation', () => {
     let mainPage;
 
     test.beforeEach(async ({ page }) => {
@@ -10,39 +10,39 @@ test.describe('Login Page Validation', () => {
         await mainPage.goto(); 
     });
 
-    test('Login with valid login and password fields', async () => {
+    test('Login with valid data / TC 1', async () => {
         await mainPage.fillLoginDetails('TestLogin_', 'Password1@');
         await mainPage.clickLoginButton();
         await mainPage.clickLogoutButton();
     });
 
-    test('Login with invalid login and password fields', async () => {
-        const invalidLogin = faker.string.alphanumeric(8); 
-        const invalidPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
-
-        await mainPage.fillLoginDetails(invalidLogin, invalidPassword);
-        await mainPage.clickLoginButton();
-        await mainPage.checkWarningLabel();
-    });
-
-    test('Login with empty login and password fields', async () => {
-        await mainPage.fillLoginDetails('', ''); 
+    test('Login with empty Login field / TC 2', async () => {
+        const validPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
+         
+        await mainPage.fillLoginDetails('', validPassword); 
         await mainPage.clickLoginButton();
         await mainPage.errorMessageLogin();
     });
 
-    test('Login with only valid login field', async () => {
+    test('Login with empty Password field / TC 3', async () => {
         const validLogin = faker.string.alphanumeric(8); 
         await mainPage.fillLoginDetails(validLogin, ''); 
         await mainPage.clickLoginButton();
         await mainPage.errorMessagePassword();
     });
 
-    test('Login with only valid password field', async () => {
-        const validPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
-         
-        await mainPage.fillLoginDetails('', validPassword); 
+    test('Login with empty fields / TC 4', async () => {
+        await mainPage.fillLoginDetails('', ''); 
         await mainPage.clickLoginButton();
         await mainPage.errorMessageLogin();
+    });
+
+    test('Login with invalid data / TC 5', async () => {
+        const invalidLogin = faker.string.alphanumeric(8); 
+        const invalidPassword = faker.internet.password({ length: 4, memorable: false, pattern: /[a-zA-Z0-9]/ });
+
+        await mainPage.fillLoginDetails(invalidLogin, invalidPassword);
+        await mainPage.clickLoginButton();
+        await mainPage.checkWarningLabel();
     });
 });
