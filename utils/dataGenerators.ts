b57196172a77a4d9124577f945f8723304d4e2e8
SerhiiQA <nabukve@gmail.com>
2025-03-14 11:12:29 +0200

    delete comments

--- utils/dataGenerators.ts
+++ utils/dataGenerators.ts
@@ -2,38 +2,31 @@ import { faker } from '@faker-js/faker';
 import * as fs from 'fs';
 import * as path from 'path';
 
-// Шлях до файлу userData.json
 const filePath = path.resolve(__dirname, './userData.json');
 
-// Функція для запису останнього створеного користувача
 export const saveLastRegisteredUser = (user: { login: string; password: string; firstName: string; lastName: string }) => {
-    fs.writeFileSync(filePath, JSON.stringify(user, null, 2)); // Перезаписуємо файл з новими даними
+    fs.writeFileSync(filePath, JSON.stringify(user, null, 2)); 
 };
 
-// Функція для отримання останнього зареєстрованого користувача
 export const getLastRegisteredUser = (): { login: string; password: string; firstName: string; lastName: string } | null => {
     if (!fs.existsSync(filePath)) {
-        return null; // Якщо файл не існує, повертаємо null
+        return null; 
     }
     const fileData = fs.readFileSync(filePath, 'utf-8');
-    return JSON.parse(fileData); // Повертаємо об'єкт користувача
+    return JSON.parse(fileData); 
 };
 
-// Генерація та збереження логіна
-export const generateAndStoreValidLogin = (): string => faker.string.alphanumeric(8); // Генеруємо випадковий алфавітно-цифровий логін
+export const generateAndStoreValidLogin = (): string => faker.string.alphanumeric(8); 
 
-// Генерація та збереження пароля
 export const generateAndStoreValidPassword = (): string => {
     const passwordLength = 8;
     return faker.internet.password({
         length: passwordLength,
         memorable: false,
         pattern: /[a-zA-Z0-9!@#$%^&*()]/,
-    }) + '1!Serg'; // Ускладнюємо пароль
+    }) + '1!Serg'; 
 };
 
-// Генерація випадкового імені
-export const generateValidFirstName = (): string => faker.person.firstName(); // Використовуємо faker для імені
+export const generateValidFirstName = (): string => faker.person.firstName(); 
 
-// Генерація випадкового прізвища
-export const generateValidLastName = (): string => faker.person.lastName(); // Використовуємо faker для прізвища
+export const generateValidLastName = (): string => faker.person.lastName(); 
