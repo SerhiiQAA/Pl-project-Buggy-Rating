7d6b61d608213dc0c37d25fe0e8b92e112a52ca7
SerhiiQA <nabukve@gmail.com>
2025-03-14 13:06:54 +0200

    debug

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
     ['list'],
     ['allure-playwright', { outputFolder: 'allure-results' }]
   ],
-  
+    
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
